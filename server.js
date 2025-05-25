2630d2dd5779c26357080e0f4e9263f5ef70d3cd
Jai Dhingra <117927011+jaidh01@users.noreply.github.com>
2025-05-25 17:58:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
       'http://localhost:3000',
       'http://localhost:5173',
       'https://law-livid-theta.vercel.app',
-      'https://law-frontend.vercel.app',
+      /^https:\/\/law.*\.vercel\.app$/
       'https://law-backend-44pr.onrender.com'
     ];
 
